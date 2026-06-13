f9020022084be07e05cd6a7f122951aed6e618b5
ayan from laptop <ayanali.sayed2006@gmail.com>
2026-06-13 20:41:14 +0530

    java basics added

---
+++ java/basics/basics.java
@@ -0,0 +1,10 @@
+class FirstClass {
+	public static void main(String args[]){
+		System.out.println("hellorld");
+		int a = 10; String raj = "rajesh pal";
+		double b = 10.10;
+		System.out.println(a);		
+		System.out.println(raj);
+		System.out.println(b);
+	}
+}
